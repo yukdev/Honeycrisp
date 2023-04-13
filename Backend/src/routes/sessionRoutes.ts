b5b52e20ec1a838cd6e9e8a2418db31496443239
Ken Yu <ken.yuwork@gmail.com>
2023-04-13 15:10:32 -0400

    adjusted session item logic

--- Backend/src/routes/sessionRoutes.ts
+++ Backend/src/routes/sessionRoutes.ts
@@ -5,6 +5,7 @@ import { requireAdmin } from '../middleware/auth';
 import { BadRequestError, NotFoundError } from '../expressErrors';
 import sessionCreate from '../schemas/sessionCreate.json';
 import sessionItemsEaten from '../schemas/sessionItemsEaten.json';
+import sessionItemAdd from '../schemas/sessionItemAdd.json';
 import calculateBill from '../helpers/calculateBill';
 
 const router = Router();
@@ -107,12 +108,67 @@ router.get('/:sessionId', async (req, res, next) => {
   }
 });
 
+router.post('/:sessionId/add', async (req, res, next) => {
+  try {
+    const { sessionId } = req.params;
+    const { items } = req.body;
+
+    const session = await prisma.session.findUnique({
+      where: { id: sessionId },
+      include: { items: true },
+    });
+
+    if (!session) {
+      throw new NotFoundError(`Session not found with id ${sessionId}`);
+    }
+
+    // Check if session has already been finalized
+    if (session.finalized) {
+      throw new BadRequestError(
+        `Session with id ${sessionId} has already been finalized`,
+      );
+    }
+
+    const validator = validate(req.body, sessionItemAdd, { required: true });
+
+    if (!validator.valid) {
+      const errors = validator.errors.map((e) => e.stack).join('\n');
+      throw new BadRequestError(errors);
+    }
+
+    const itemsWithQuantity = [];
+
+    for (const item of items) {
+      for (let i = 0; i < item.quantity; i++) {
+        itemsWithQuantity.push({
+          name: item.name,
+          price: item.price,
+        });
+      }
+    }
+
+    const updatedSession = await prisma.session.update({
+      where: { id: sessionId },
+      include: { items: true },
+      data: {
+        items: {
+          create: itemsWithQuantity,
+        },
+      },
+    });
+
+    res.json(updatedSession);
+  } catch (err) {
+    next(err);
+  }
+});
+
 /**
  * PUT /sessions/:sessionId
  *
  * Marks items as eaten for a session.
  */
-router.put('/:sessionId', async (req, res, next) => {
+router.put('/:sessionId/eat', async (req, res, next) => {
   try {
     const { sessionId } = req.params;
     const { items } = req.body;
@@ -130,11 +186,11 @@ router.put('/:sessionId', async (req, res, next) => {
     }
 
     // Check if session has already been finalized
-    if (session.finalized) {
-      throw new BadRequestError(
-        `Session with id ${sessionId} has already been finalized`,
-      );
-    }
+    // if (session.finalized) {
+    //   throw new BadRequestError(
+    //     `Session with id ${sessionId} has already been finalized`,
+    //   );
+    // }
 
     // Validate request body
     const validator = validate(req.body, sessionItemsEaten, { required: true });
@@ -183,11 +239,11 @@ router.post('/:sessionId/finalize', async (req, res, next) => {
     }
 
     // Check if session has already been finalized
-    if (session.finalized) {
-      throw new BadRequestError(
-        `Session with id ${sessionId} has already been finalized`,
-      );
-    }
+    // if (session.finalized) {
+    //   throw new BadRequestError(
+    //     `Session with id ${sessionId} has already been finalized`,
+    //   );
+    // }
 
     // Check if it's the owner finalizing the session
     if (session.ownerId !== user.id) {
