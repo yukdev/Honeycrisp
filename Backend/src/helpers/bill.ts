0d5a81d6c5daa5a84b301039993b793eeae36e76
Ken Yu <ken.yuwork@gmail.com>
2023-05-12 13:23:15 -0400

    revised calculateBill for correct flat tip split

--- Backend/src/helpers/bill.ts
+++ Backend/src/helpers/bill.ts
@@ -6,13 +6,19 @@ export function calculateBill(
   tax: number,
   tip: number,
   tipType: TipType,
-): number {
+): {
+  subtotal: number;
+  bill: number;
+} {
   const subtotal = items.reduce((acc, item) => acc + item.price, 0);
   const taxAmount = subtotal * (tax / 100);
   const tipAmount =
     tipType === TipType.PERCENTAGE ? subtotal * (tip / 100) : tip;
   const total = subtotal + taxAmount + tipAmount;
-  return Number(total.toFixed(2));
+  return {
+    subtotal: Number(subtotal.toFixed(2)),
+    bill: Number(total.toFixed(2)),
+  };
 }
 
 export function calculateSplit(session: Session): {
@@ -20,34 +26,37 @@ export function calculateSplit(session: Session): {
   name: string;
   split: number;
 }[] {
-  const userBills: Record<string, number> = {};
-  const { items, bill, tax, tip, tipType, ownerId } = session;
-
+  const { items, subtotal, tax, tip, tipType, ownerId } = session;
   const taxPercent = tax / 100;
-  const tipPercent = tipType === TipType.PERCENTAGE ? tip / 100 : tip / bill;
+  const tipPercent =
+    tipType === TipType.PERCENTAGE ? tip / 100 : tip / subtotal;
 
-  items.forEach((item) => {
-    const numEaters = item.userItems.length;
-    const itemCost = item.price / numEaters;
+  const userBills = items.reduce((acc, item) => {
+    const itemCost = item.price / item.userItems.length;
+
     item.userItems.forEach((userItem) => {
-      const userId = userItem.user.id;
-      const currentTotal = userBills[userId] || 0;
-      userBills[userId] = currentTotal + itemCost;
+      const { id } = userItem.user;
+      acc[id] = (acc[id] || 0) + itemCost;
     });
-  });
 
-  const split: Record<string, number> = {};
-  Object.entries(userBills).forEach(([userId, subtotal]) => {
-    let total = subtotal * (1 + taxPercent) * (1 + tipPercent);
-    split[userId] = roundToTwoDecimals(total);
-  });
+    return acc;
+  }, {} as Record<string, number>);
+
+  const split = Object.entries(userBills).reduce((acc, [userId, subtotal]) => {
+    const total = roundToTwoDecimals(subtotal * (1 + taxPercent + tipPercent));
+    acc[userId] = total;
+    return acc;
+  }, {} as Record<string, number>);
+
+  const userMap = new Map<string, string>(
+    session.items.flatMap((item) =>
+      item.userItems.map((userItem) => [userItem.user.id, userItem.user.name]),
+    ),
+  );
 
   return Object.entries(split).map(([id, split]) => ({
     id,
-    name:
-      session.items
-        .flatMap((item) => item.userItems)
-        .find((userItem) => userItem.user.id === id)?.user.name || '',
+    name: userMap.get(id) || '',
     split,
     paid: id === ownerId,
   }));
