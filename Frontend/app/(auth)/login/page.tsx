a9055a963858973afabcae7ba7d4e9ffd73b4aaf
Ken Yu <ken.yuwork@gmail.com>
2023-05-19 14:54:07 -0400

    redirect logged in users from register/login if

--- Frontend/app/(auth)/login/page.tsx
+++ Frontend/app/(auth)/login/page.tsx
@@ -1,6 +1,14 @@
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../../api/auth/[...nextauth]/route';
 import AuthForm from '@/components/AuthForm';
-
+import UnauthorizedPage from '@/components/UnauthorizedPage';
 const Login = async () => {
+  const userSession = (await getServerSession(authOptions)) as any;
+
+  if (userSession?.user) {
+    return <UnauthorizedPage errorMessage="You are already logged in." />;
+  }
+
   return <AuthForm mode="login" />;
 };
 
--- Frontend/app/(auth)/register/page.tsx
+++ Frontend/app/(auth)/register/page.tsx
@@ -1,6 +1,15 @@
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../../api/auth/[...nextauth]/route';
 import AuthForm from '@/components/AuthForm';
+import UnauthorizedPage from '@/components/UnauthorizedPage';
 
-const Register = () => {
+const Register = async () => {
+  const userSession = (await getServerSession(authOptions)) as any;
+
+  if (userSession?.user) {
+    return <UnauthorizedPage errorMessage="You are already logged in." />;
+  }
+
   return <AuthForm mode="register" />;
 };
 
